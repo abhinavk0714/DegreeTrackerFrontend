ca7cc816b91f74daa7b6005cc93aa5d006ad6d2d
Abhinav Krishniah <abhinavk@email.sc.edu>
2024-04-09 14:29:03 -0400

    working on LoginController

--- src/main/java/controllers/LoginController.java
+++ src/main/java/controllers/LoginController.java
@@ -1,13 +1,47 @@
 package controllers;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import library.App;
+import model.*;
 
-public class LoginController {
+public class LoginController implements Initializable {
+    
+    //private static GradeSystemFACADE facade;
+    private UserList userList;
+    private User currentUser;
+    private CourseList courseList;
+    private MajorList majorList;
 
     @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("Login");
+    private TextField txt_username;
+    @FXML
+    private TextField txt_password;
+    @FXML
+    private Label lbl_error;
+
+    @FXML
+    private void btnLoginClicked(MouseEvent event) throws IOException {
+        String username = txt_username.getText();
+        String password = txt_password.getText();
+
+        GradeSystemFACADE gradeSystemFACADE = GradeSystemFACADE.getFacadeInstance(userList, courseList, majorList);
+
+        if (!gradeSystemFACADE.login(username, password)) {
+            lbl_error.setText("Invalid login credentials.");
+            return;
+        }
+
+        App.setRoot(""); //student view or advisor view, needs dual support
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
     }
 }

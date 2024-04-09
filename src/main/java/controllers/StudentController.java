6d99dff19223f6602685702a45de6173ba1c1073
tlilly5 <tlilly@email.sc.edu>
2024-04-09 19:12:33 -0400

    added more in student view controller

--- src/main/java/controllers/StudentController.java
+++ src/main/java/controllers/StudentController.java
@@ -11,6 +11,8 @@ import model.*;
 public class StudentController implements Initializable{
 
     @FXML private Label screen_title;
+    private GradeSystemFACADE facade;
+    private User user;
     private UserList userList;
     private User currentUser;
     private CourseList courseList;
@@ -18,18 +20,35 @@ public class StudentController implements Initializable{
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        GradeSystemFACADE facade = GradeSystemFACADE.getFacadeInstance(userList, courseList,majorList);
+        GradeSystemFACADE facade = GradeSystemFACADE.getFacadeInstance(userList, courseList, majorList);
         User user = currentUser;
         
         screen_title.setText("Welcome " + user.getFirstName() + " " + user.getLastName());
         
         getUserItems();
+        getEightSemesterPlan();
     }
 
     private void getUserItems()
     {
         VBox vbox = new VBox();
-        
+        Student currStudent = facade.findStudent(user.getUsername());
+        Label studentID = new Label(currStudent.getUUID().toString());
+        Label classifaction = new Label(currStudent.getClassification().toString());
+        Label major = new Label(currStudent.getMajor().getMajorName());
+        //Label appArea = new Label(); not sure how to do this one
+        Label advisor = new Label(currStudent.getAdvisor().getFirstName() + " " + currStudent.getAdvisor().getLastName());
+        Label gpa= new Label(String.valueOf(currStudent.getOverallGPA()));
+        Label flag = new Label(currStudent.getFlag().toString());
+        //Label currentSemester = new Label()
+        Label FERPA = new Label(currStudent.getFEPRA().toString());
+
+
+    }
+
+    private void getEightSemesterPlan()
+    {
+        EightSemesterPlan courses = facade.findStudent(user.getUsername()).getEightSemesterPlan();
 
     }
    

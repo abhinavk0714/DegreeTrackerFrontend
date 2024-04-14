8a3817e43475c246a61c684761cce6ba9e8367a1
LibaanM <lmohamed@email.sc.edu>
2024-04-14 18:56:36 -0400

    fixing course

--- src/main/java/controllers/CourseController.java
+++ src/main/java/controllers/CourseController.java
@@ -1,13 +1,45 @@
 package controllers;
 
 import java.io.IOException;
-import javafx.fxml.FXML;
-import library.App;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.ResourceBundle;
+import java.util.UUID;
 
-public class CourseController {
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.fxml.Initializable;
+import library.App;
+import model.Availablity;
+
+public class CourseController implements Initializable{
+    private UUID id;
+    private String name;
+    private String department;
+    private String number;
+    private String description;
+    private long creditHours;
+    private ArrayList<Availablity> availablity;
+    private HashMap<UUID, String> prerequisite;
+    private ArrayList<UUID> corequisite;
+
+    @FXML
+    private TextField txt_department;
+    
+    @FXML
+    private TextField txt_courseNumber;
+   
+    @FXML
+    private Button searchButton;
 
     @FXML
     private void switchToPrimary() throws IOException {
         App.setRoot("Course");
+    }
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
     }
 }

1a41ff761e00fc073aaccb428d6e8298ad453c9b
tlilly5 <tlilly@email.sc.edu>
2024-04-18 14:12:26 -0400

    changed back to log out in ad and stud

--- src/main/java/controllers/StudentController.java
+++ src/main/java/controllers/StudentController.java
@@ -35,7 +35,7 @@ public class StudentController {
     private URL location;
 
     @FXML
-    private Button backButton;
+    private Button LOButton;
 
     @FXML
     private Button courseSearchButton;

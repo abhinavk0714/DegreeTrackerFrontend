bcff65925573e95e39f3942a9c425b684712f3cf
tlilly5 <tlilly@email.sc.edu>
2024-04-21 13:35:29 -0400

    trying to pull

--- src/main/java/controllers/CourseController.java
+++ src/main/java/controllers/CourseController.java
@@ -10,7 +10,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
-import javafx.scene.text.Text;
 import javafx.fxml.Initializable;
 import library.App;
 import model.CourseList;
